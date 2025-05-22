17eb064ee0be4db90c50011aea1e657cf2b9a61f
Shruti-Kulkarni20 <shrutikulkarni503@gmail.com>
2025-05-22 13:53:22 +0530

    Add files via upload

---
+++ Assignment_16/programA2.c
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// File name  : programA2.c
+// Description: program which accept number of rows and colums from user and display below pattern.
+// Input      : iRow = 4    iCol = 4
+// Output     : * * * *
+//              * * * 
+//              * * 
+//              *
+// Author     : Shruti S Kulkarni
+// Date       : 22/05/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow , int iCol)
+{
+  int i = 0, j = 0;
+  if (iRow != iCol)
+  {
+    printf("Invalid Output");
+  }
+  
+  for(i = 1; i <= iRow; i++)
+  {
+    for(j = iCol;j >= 1; j--)
+    {
+        if(j >= i)
+        {
+          printf("*\t");
+        }
+    }
+    printf("\n");
+  }
+
+}
+int main()
+{
+
+    int iValue1 = 0 , iValue2 = 0;
+
+    printf("enter number of rows and columns : ");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
