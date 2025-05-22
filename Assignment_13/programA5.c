47d2bc0d42d7f03a5aa4b0d96a7952bd6fd9752e
Shruti-Kulkarni20 <shrutikulkarni503@gmail.com>
2025-05-22 13:48:00 +0530

    Update programA5.c

--- Assignment_13/programA5.c
+++ Assignment_13/programA5.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // 
-// File name  : programA1.c
+// File name  : programA5.c
 // Description: program which accept number of rows and colums from user and display below pattern.
 // Input      : iRow = 3    iCol = 4
 // Output     : 1 2 3 4
